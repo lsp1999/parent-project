3a37e77f54b2f42b74009538eb454af9b9b7159b
lsp1999 <3031353738@qq.com>
2019-12-20 10:16:18 +0800

    test

---
+++ eureka-server/src/main/java/org/example/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @date: 2019/12/19 16:28
+ * @email: 3031353738@qq.com
+ * @author: liushupeng
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
---
+++ user-module/src/main/java/org/example/UserApplication.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @date: 2019/12/19 17:11
+ * @email: 3031353738@qq.com
+ * @author: liushupeng
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
