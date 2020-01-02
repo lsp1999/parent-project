145f6b4b8c5cc1163bc6db7564a227280d5228b1
lsp1999 <3031353738@qq.com>
2020-01-02 17:53:55 +0800

    test

---
+++ zuul-gateway/src/main/java/org/example/filters/PreFilter.java
@@ -0,0 +1,45 @@
+package org.example.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @date: 2020/1/2 14:30
+ * @email: 3031353738@qq.com
+ * @author: liushupeng
+ * @description: filterType:pre在路由请求之前执行过滤器
+ */
+public class PreFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+
+}
